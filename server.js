0b8780bb265a21feb1cf7e5b759f235e42c3f592
Your Name <you@example.com>
2026-08-18 17:20:14 +0530

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ mongoose.connect(mongoUri)
   });
 
 // ✅ Server Start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
